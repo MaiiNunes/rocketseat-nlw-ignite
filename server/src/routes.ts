2dd71e51478375a7acb6a27709f09607f9e50dda
Maísa T. Nunes <maisa.t.nunes@gmail.com>
2023-01-19 11:52:50 -0300

    updt.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -75,4 +75,76 @@ export async function appRoutes(app: FastifyInstance) {
             completedHabits,
         }
     })
+
+    //completar / desmarcar hábito feito.
+
+    app.patch('/habits/:id/toggle', async (request) => {
+        //route param => parâmetro de identificação
+
+        const toggleHabitParams = z.object({
+            id: z.string().uuid(),
+        })
+
+        const {id} = toggleHabitParams.parse(request.params)
+
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where:{
+                date: today,
+            }
+        })
+
+        if(!day) {
+            day = await prisma.day.create({
+                data:{
+                    date: today,
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where:{
+                day_id_habit_id:{
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            }
+        })
+
+        if (dayHabit){
+            await prisma.dayHabit.delete({
+                where:{
+                    id: dayHabit.id,
+                }
+            })
+        } else {
+            await prisma.dayHabit.create({
+                data:{
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            })
+        }
+    })
+
+    app.get('/summary', async () => {
+        //Query mais complexa, mais condições, relacionamentos => SQL na mão (RAW)
+        //Prisma ORM: RAW SQL => SQLite (somente o SQL irá interpretar o RAW, pois cada banco escrito a mão tem uma sintaxe diferente)
+
+        const summary = await prisma.$queryRaw`
+            SELECT 
+                D.id,
+                D.date,
+                (
+                    SELECT
+                        cast(count(*) as float)
+                    FROM day_habits DH
+                    WHERE DH.day_id = D.id
+                ) as completed
+            FROM days D
+        `
+
+        return summary
+    })
 }
