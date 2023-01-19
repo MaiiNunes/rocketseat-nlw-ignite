aebd6797ee779db689912c145e2afc59e77add19
Maísa T. Nunes <maisa.t.nunes@gmail.com>
2023-01-19 20:20:06 -0300

    aula-3 mobile 3/3

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -22,7 +22,7 @@ export function New(){
 
     return(
         <View className="flex-1 bg-background px-8 pt-16">
-            <ScrollView showsVerticalScrollIndicator={false}>
+            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 100 }}>
 
                 <BackButton/>
 
